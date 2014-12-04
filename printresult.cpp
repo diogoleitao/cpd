064bd735c6adc7564f4b33705687b588658cfbfd
Ana Costeira Araujo <ana@Ana.(none)>
2014-12-04 19:15:29 +0000

    small modifications from previous commit. just slittly better code, i think

--- printresult.cpp
+++ printresult.cpp
@@ -2,6 +2,8 @@ void computeResult(int j, int id){
  
 	int i = N_LENGTH; //LAST POSITION   - ASSUMING THIS VARIABLES ARE UPDATED FOR EACH PROCESS!
 	string result;
+	string tmp;
+	string final_result;
 
 	while(i != 0){
 			if(N[i] == M[j]){ //MATCH
@@ -13,7 +15,7 @@ void computeResult(int j, int id){
 				j--;
 			else
 				i--;
-		}
+	}
 
 	if (id != 0){
 		int size = result.size();
@@ -23,8 +25,16 @@ void computeResult(int j, int id){
 
 	}
 	else{
-		cout << result.size() << endl;
-		cout << result << endl;
+
+		for(int k = p; i > 0; i++){
+			MPI_Recv($length, 1, MPI_INT, p, 5, MPI_COMM_WORLD, &status ); //RECV STRLEN FROM 3 PROCESSORS
+			MPI_Recv(tmp, lenght, MPI_CHAR, p, 6, MPI_COMM_WORLD, &status); //RECV STR FROM 3 PROCESSORS
+			final_result.insert(0, tmp); //JOIN ALL STR;
+		}
+
+		final_result.insert(0, result);
+		cout << final_result.size() << endl;
+		cout << final_result << endl;
 	}
 
 }
@@ -36,25 +46,12 @@ void printResult(int id, int p){
 
 	int i = N_LENGTH; 
 	int j = M_LENGTH; //LAST POSITION
-	string result;
-	string final_result;
 	int length;
 
 	if(id == p){ //starts with the last processor!
 		computeResult(j, id);
 	}
-	else if(id == 0){ // IS MAIN PROCESSOR
-		for(int k = p; i > 0; i++){
-			MPI_Recv($length, 1, MPI_INT, p, 5, MPI_COMM_WORLD, &status ); //RECV STRLEN FROM 3 PROCESSORS
-			MPI_Recv(result, lenght, MPI_CHAR, p, 6, MPI_COMM_WORLD, &status); //RECV STR FROM 3 PROCESSORS
-			final_result.insert(0, result); //JOIN ALL STR;
-		}
-
-		MPI_Recv(&j, 1, MPI_INT, id-1, 7, MPI_COMM_WORLD, $status);//RECEIVE COLUMN
-		computeResult(j, id);//PERFORM ALGORITHM.
-
-	}
-	else{
+	else {
 		MPI_Recv(&j, 1, MPI_INT, id-1, 7, MPI_COMM_WORLD, $status);//RECEIVE COLUMN
 		computeResult(j, id);//PERFORM ALGORITHM.
 	}
