8a40eb196f87710ba35ef17b77672135ad29585a
DiogoRosado <diogo.rosado@ist.utl.pt>
2014-12-05 00:57:36 +0000

    provavelmente final - falta testar nos labs

--- lcs-mpi.cpp
+++ lcs-mpi.cpp
@@ -5,12 +5,13 @@
  *  
  */
 
-#include <mpi.h>
 #include <stdio.h>
+#include <string.h>
 #include <string>
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
+#include <mpi.h>
 
 using namespace std;
 
@@ -19,10 +20,15 @@ using namespace std;
  ***********************/
 
 //Direccions
-int MATCH = 0;
-int UP = 1;
-int LEFT = 2;
- 
+#define MATCH 0
+#define UP 1
+#define LEFT 2
+#define MAXWIDTH 128
+
+//MPI variables
+	MPI_Status status;
+  int id, p;
+
 //Input strings
 char* N;
 char* M;
@@ -33,12 +39,21 @@ int M_LENGTH;
 
 //Auxiliary tables
 short** TABLE;
-short** TRACKER;
 
 
 /***********************
  ** AUXILIARY PROCEDURES
  ***********************/
+
+//Prints the auxiliary table
+void printTable() {
+	for (int i = 0; i < N_LENGTH; i++) {
+		for (int j = 0; j < M_LENGTH; j++)
+			printf(" %d |", TABLE[i][j]);
+		printf("\n");
+	}
+	printf("\n\n");
+}
 
 //The cost routine
 short cost(int x) {
@@ -51,190 +66,164 @@ short cost(int x) {
 	return (short) (dcost / n_iter + 0.1);
 }
 
-//Reads an input file with a given name and initializes global variables
-void initProblem() {
-	int i = 0;
-	int correctedSizeN = N_LENGTH + 1;
-	int correctedSizeM = M_LENGTH + 1;
+//Reads the input file
+int readFile(string filename){		
+	FILE *myfile;
+	myfile = fopen(filename.c_str(), "r");
 
-	//Create auxiliary tables
-	TABLE = new short*[correctedSizeN];
-	TRACKER = new short*[correctedSizeN];
+	if (myfile != NULL){
+		if (fscanf(myfile, "%d %d", &N_LENGTH, &M_LENGTH) <= 0) {
+			return 0;
+		}
 
-	for (i = 0; i < N_LENGTH + 1; ++i) {
-		TABLE[i] = new short[correctedSizeM]();
-		TRACKER[i] = new short[correctedSizeM]();
+		//Create the arrays to store the strings;
+		N = new char[N_LENGTH+1];
+		M = new char[M_LENGTH+1];
+
+		if (fscanf(myfile, "%s", N) <= 0) {
+			return 0;
+		}
+		if (fscanf(myfile, "%s", M) <= 0) {
+			return 0;
+		}
+
+	} else {
+		return 0;
 	}
+
+	return 1;
 }
 
-//Computes the solution using an auxiliary table
-void computeSolution() {
-	int i = 0, j = 0;
-	short n, m;
+void splitLines(){
+	int rl = N_LENGTH; //remaining lines to split
+	int rp = p; //remaining processes
+	
+	//split to process 0
+	int index = 0;
+	int size = floor(rl/rp);
+	N_LENGTH = size;
+	rl -= size;
+	rp--;
 
-	for (i = 1; i <= N_LENGTH; i++) {
-		for (j = 1; j <= M_LENGTH; j++) {
-			if (N[i-1] == M[j-1]) {
+	//split to other processes
+	for(int i = 1; i < p; i++){
+		index += size;
+		size = floor(rl/rp);
+		MPI_Send(&size, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+		MPI_Send(&M_LENGTH, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+		MPI_Send(N+index, size, MPI_CHAR, i, 3, MPI_COMM_WORLD);		
+		MPI_Send(M, M_LENGTH, MPI_CHAR, i, 4, MPI_COMM_WORLD);
+		rl -= size;
+		rp--;
+	}	
+}
+
+//Initializes the auxiliary table
+void initTable(int nlines, int ncols){
+	TABLE = new short*[nlines];
+	for (int i = 0; i < nlines; TABLE[i++] = new short[ncols]());
+}
+
+void computeBlock(int index, int size) {
+	int i,j;
+	int limit = index+size;
+
+	for (i = 1; i < N_LENGTH; i++) {
+		for (j = index; j < limit; j++) {
+			if (N[i-1] == M[j-1])
 				TABLE[i][j] = TABLE[i-1][j-1] + cost(j);
-				//DIAGONAL - IT'S A MATCH
-				TRACKER[i][j] = MATCH;
-			} else if (TABLE[i-1][j] > TABLE[i][j-1]) {
+			else if (TABLE[i-1][j] > TABLE[i][j-1])
 				TABLE[i][j] = TABLE[i-1][j];
-				//UP - FETCH VALUE FROM PREVIOUS SUBPROBLEM
-				TRACKER[i][j] = UP;
-			} else {
+			else 
 				TABLE[i][j] = TABLE[i][j-1];
-				//LEFT - FETCH VALUE FROM CURRENT SUBPROBLEM
-				TRACKER[i][j] = LEFT;
-			}
 		}
 	}
 }
 
-//Prints the result of our previous computation
-void printResult() {
-	int length = TABLE[N_LENGTH][M_LENGTH];
+void printSolution(){
+	int i = N_LENGTH-1;
+	int j;	
+	int length;
+	int index;
+	if(id == p-1){
+		j = M_LENGTH-1;
+		length = TABLE[i][j];
+		index = length;
+	} else{
+		MPI_Recv(&length, 1, MPI_INT, id+1, 6, MPI_COMM_WORLD, &status);
+		MPI_Recv(&index, 1, MPI_INT, id+1, 7, MPI_COMM_WORLD, &status);
+		MPI_Recv(&j, 1, MPI_INT, id+1, 8, MPI_COMM_WORLD, &status);
+	}
+	
 	char result[length + 1];
-	result[length--] = '\0';
-
-	//Prints the size of the biggest subsequence
-	cout << TABLE[N_LENGTH][M_LENGTH] << endl;
-
-	//Tracks the biggest subsequence
-	int i = N_LENGTH;
-	int j = M_LENGTH;
-
-	while (length >= 0) {
-		if (TRACKER[i][j] == MATCH) {
-			result[length--] = N[i-1];
+	
+	if(id == p-1) result[index--] = '\0';
+	else MPI_Recv(result+index+1, length-index, MPI_CHAR, id+1, 9, MPI_COMM_WORLD, &status);
+	
+	while(i > 0 && j > 0){
+		if(N[i-1] == M[j-1]){
+			result[index--] = N[i-1];
 			--i;
 			--j;
-		} else if (TRACKER[i][j] == UP)
+		}
+		else if(TABLE[i-1][j] > TABLE[i][j-1])
 			--i;
 		else
 			--j;
 	}
-
-	cout << result << endl;
+	
+	if(id == 0){
+		cout << length << endl;;
+		cout << result << endl;
+	} else {
+		MPI_Send(&length, 1, MPI_INT, id-1, 6, MPI_COMM_WORLD);
+		MPI_Send(&index, 1, MPI_INT, id-1, 7, MPI_COMM_WORLD);
+		MPI_Send(&j, 1, MPI_INT, id-1, 8, MPI_COMM_WORLD);
+		MPI_Send(result+index+1, length-index, MPI_CHAR, id-1, 9, MPI_COMM_WORLD);
+	}	
 }
-
 
 /***********************
  ** MAIN
  ***********************/
-
 int main (int argc, char *argv[]) {
 
-	MPI_Status status;
-	int id, p, i;
-	double secs;
-
+	//initialize MPI
 	MPI_Init (&argc, &argv);
-
-	MPI_Comm_rank (MPI_COMM_WORLD, &id);
-	MPI_Comm_size (MPI_COMM_WORLD, &p);
-  
-	if (!id) {
-		FILE *myfile;
-		string filename = argv[1];
-		myfile = fopen(filename.c_str(), "r");
-
-		if (myfile != NULL) {
-			if (fscanf(myfile, "%d %d", &N_LENGTH, &M_LENGTH) <= 0)
-				exit(1);
-
-			//Create the arrays to store the strings;
-			N = new char[N_LENGTH + 1];
-			M = new char[M_LENGTH + 1];
-
-			if (fscanf(myfile, "%s", N) <= 0)
-				exit(1);
-			if (fscanf(myfile, "%s", M) <= 0)
-				exit(1);
-
-			//Divide input
-			if(!even){
-				PM_LENGTH = floor(M_LENGTH / p);
-				for (i = 1; i < p; i++) {
-					PM_index = i*PM_LENGTH;
-					if(i == p-1) PM_LENGTH = M_LENGTH - (p-1)*PM_LENGTH;					
-					MPI_Send(&N_LENGTH, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-					MPI_Send(&PM_LENGTH, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-					MPI_Send(N, N_LENGTH, MPI_CHAR, i, 2, MPI_COMM_WORLD);
-					MPI_Send((M + PM_index), PM_LENGTH, MPI_CHAR, i, 3, MPI_COMM_WORLD);
-				}
-				M_LENGTH = floor(M_LENGTH / p); 
-
-			} else {
-				PM_LENGTH = floor(M_LENGTH / p);
-				PN_LENGTH = floor(N_LENGTH/2);
-				for(i = 1; i < p/2; i++) {
-					PM_index = i*PM_LENGTH;
-					if(i == (p/2)-1) PM_LENGTH = M_LENGTH - ((p/2)-1)*PM_LENGTH;					
-					MPI_Send(&PN_LENGTH, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-					MPI_Send(&PM_LENGTH, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-					MPI_Send(N, PN_LENGTH, MPI_CHAR, i, 2, MPI_COMM_WORLD);
-					MPI_Send((M + PM_index), PM_LENGTH, MPI_CHAR, i, 3, MPI_COMM_WORLD);
-				}
-				PM_LENGTH = floor(M_LENGTH / p);
-				PN_index = PN_LENGTH; 
-				PN_LENGTH = N_LENGTH - PN_LENGTH;
-				for(; i < p; i++){
-					PM_index = (i-p/2)*PM_LENGTH;
-					if(i == p-1) PM_LENGTH = M_LENGTH - ((p/2)-1)*PM_LENGTH;					
-					MPI_Send(&PN_LENGTH, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-					MPI_Send(&PM_LENGTH, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-					MPI_Send((N + PN_index), PN_LENGTH, MPI_CHAR, i, 2, MPI_COMM_WORLD);
-					MPI_Send((M + PM_index), PM_LENGTH, MPI_CHAR, i, 3, MPI_COMM_WORLD);
-				}
-				M_LENGTH = floor(M_LENGTH / p);
-			}
-
-			/*//Envia informação aos outros processos
-			for (i = 1; i < p; i++) {
-				MPI_Send(&N_LENGTH, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-				MPI_Send(&M_LENGTH, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-				MPI_Send(N, N_LENGTH + 1, MPI_CHAR, i, 2, MPI_COMM_WORLD);
-				MPI_Send(M, M_LENGTH + 1, MPI_CHAR, i, 3, MPI_COMM_WORLD);
-			}*/
+  MPI_Comm_rank (MPI_COMM_WORLD, &id);
+  MPI_Comm_size (MPI_COMM_WORLD, &p);
+	if(id == 0){
+		string filename = argv[1];		
+		if(readFile(filename)) {
+			splitLines();					
 		} else {
-			printf ("%s: input file not found.", filename.c_str());
 			MPI_Finalize();
-			exit(1);
+			return 0;
 		}
-
 	} else {
-		MPI_Recv(&N_LENGTH, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-		MPI_Recv(&M_LENGTH, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-	
-		//Create the arrays to store the strings;
-		N = new char[N_LENGTH + 1];
-		M = new char[M_LENGTH + 1];
-
-		MPI_Recv(N, N_LENGTH, MPI_CHAR, 0, 2, MPI_COMM_WORLD, &status);
-		MPI_Recv(M, M_LENGTH, MPI_CHAR, 0, 3, MPI_COMM_WORLD, &status);
+		MPI_Recv(&N_LENGTH, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+		MPI_Recv(&M_LENGTH, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+		N = new char[N_LENGTH+1];
+		M = new char[M_LENGTH+1];
+		MPI_Recv(N, N_LENGTH, MPI_CHAR, 0, 3, MPI_COMM_WORLD, &status);
+		MPI_Recv(M, M_LENGTH, MPI_CHAR, 0, 4, MPI_COMM_WORLD, &status);
 	}
 
-	//Initialize	
-	initProblem();
+	N_LENGTH++; M_LENGTH++;
+	initTable(N_LENGTH, M_LENGTH);
 
-	//Compute
-  computeSolution();
+	//Compute Solution
+	int size;
+	for(int j=1; j < M_LENGTH; j += MAXWIDTH){		 
+		size = min(MAXWIDTH, M_LENGTH-j);
+		if(id != 0) MPI_Recv(&(TABLE[0][j]), size, MPI_SHORT, id-1, 5, MPI_COMM_WORLD, &status);
+		computeBlock(j, size);
+		if(id != p-1) MPI_Send(&(TABLE[N_LENGTH-1][j]), size, MPI_SHORT, id+1, 5, MPI_COMM_WORLD);
+	}
 
 	//Print
-	printResult();
- 
-	//MPI_Barrier (MPI_COMM_WORLD);
+	printSolution();
 
-    /*if (id == p-1) {
-		//Faz coisas
-    } else {
-		//Comunica
-
-		//Faz coisas
-	}*/
-
-    MPI_Finalize();
-    return 0;
+	MPI_Finalize();
+	return 0;
 }
-
